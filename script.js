51967d704499b5d6be4c29cc3566bf8512f5e1f2
George <52869708+AVxnn@users.noreply.github.com>
2021-07-07 15:25:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+let inputMoney = document.querySelector('.money-count');
+let inputBtn = document.querySelector('.add-state');
+let list = document.querySelector('.list');
+
+let state = {
+    allMoney: 0,
+    middleMoneyWeek: 0,
+    value: 0,
+    check: true,
+    state: []
+}
+
+window.onload = () => {
+    if (localStorage.getItem('state') !== null) {
+        state = JSON.parse(localStorage.getItem('state'));
+        for (let i = 1; i < state.state.length; i++) {
+            addItem(state.state[i][0], state.state[i][1], i)
+            state.value = i
+        }
+        document.querySelector('.allMoney'). innerHTML = 'Всего: ' + state.allMoney + ' руб';
+
+
+        document.querySelectorAll('.iteml').forEach(el => {
+            el.addEventListener('click', (item) => {
+
+                document.querySelector('.' + item.target.className.split(' ')[0]).innerHTML = '';
+                let x = [item.target.className.split(' ')[0].split('list-item-')[1]]
+                state.allMoney -= state.state[x][0]
+                state.state.splice(x , 1);
+                console.log(state.state)
+                localStorage.setItem('state', JSON.stringify(state))
+            })
+
+        })
+    }
+}
+
+document.querySelector('.clearr').addEventListener('click', () => {
+    window.localStorage.removeItem('state');
+    window.localStorage.clear()
+})
+
+inputBtn.addEventListener('click', function() {
+    let text = inputMoney.value.split(' ')
+    if (inputMoney.value !== '') {
+        state.allMoney += Number(text[0])
+    }
+    state.value++
+    state.state[state.value] = text;
+    addItem(Number(text[0]), text[1], state.value)
+    console.log(state.allMoney)
+    inputMoney.value = '';
+    document.querySelector('.allMoney'). innerHTML = 'Всего: ' + state.allMoney + ' руб';
+    console.log(state)
+    console.log(JSON.stringify(state))
+})
+
+function addItem(text, deadline, index) {
+    let date = new Date();
+    let datee = date.getDate() + '.' + (Number(date.getMonth()) + 1);
+    list.insertAdjacentHTML('beforeend', '<li class="list-item-' + index + ' iteml" id="item">Дата: ' + datee + ' __ Цена: ' + text + ' руб __ Срок: ' + deadline + '</li>');
+    localStorage.setItem('state', JSON.stringify(state))
+    console.log(state.state)
+}
+
